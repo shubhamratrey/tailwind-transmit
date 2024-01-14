a8a1b74ba434d58d596a2aeea1ae19dc6e842dc3
Shubham Ratery <shubhamratrey@gmail.com>
2024-01-15 02:19:05 +0530

    changes

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -177,7 +177,7 @@ export function Layout({ children }) {
 
   return (
     <>
-      <header className="bg-slate-900 lg:fixed lg:inset-y-0 lg:left-0 lg:flex lg:w-112 lg:items-start lg:overflow-y-auto xl:w-120">
+      <header className="bg-slate-100 lg:fixed lg:inset-y-0 lg:left-0 lg:flex lg:w-112 lg:items-start lg:overflow-y-auto xl:w-120">
         <div className="hidden lg:sticky lg:top-0 lg:flex lg:w-16 lg:flex-none lg:items-center lg:whitespace-nowrap lg:py-12 lg:text-sm lg:leading-7 lg:[writing-mode:vertical-rl]">
           <span className="font-mono text-slate-500">Hosted by</span>
           <span className="mt-6 flex gap-6 font-bold text-slate-900">
